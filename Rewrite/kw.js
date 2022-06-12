d8868505db0fd25e99355ba0ffeda01b9fcff645
Gui <53392694+Stride666@users.noreply.github.com>
2022-06-12 22:00:29 +0800

    KuwoMusic

--- Rewrite/kw.js
+++ Rewrite/kw.js
@@ -1,4 +1,5 @@
 /*
+KugouMusic VIP
 [rewrite_local]
 ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/Stride666/QX/master/Rewrite/kw.js
 [mitm]
